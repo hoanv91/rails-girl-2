8f3dbf5fd254eb3fb07437648739c415f7986608
hoanguyen <hoanguyen@teqnological.asia>
2019-06-24 00:33:47 +0700

    Add user login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :orders
   resources :cart_items, only: [:create, :destroy]
   resources :carts
-  resources :products
+  resources :products do
+    collection do
+      get 'list', action: :list
+    end
+  end
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
